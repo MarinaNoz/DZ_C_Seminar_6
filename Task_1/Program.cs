64d1698ef13687db3818af421cd500753bb886e0
Marina <troskom@mail.ru>
2022-05-05 15:30:54 +0300

    DZ_Seminar_6

---
+++ Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//-1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите несколько чисел через запятую ");
+string userNumber = Console.ReadLine();
+int countPositive = 0;
+
+string temp = string.Empty;
+
+for (int i = 0; i < userNumber.Length; i++)
+{
+    if ((userNumber[i] >= '0' && userNumber[i] <= '9') || (userNumber[i] == '-' && temp == string.Empty))
+        temp += userNumber[i];
+    if ((userNumber[i] == ',' || i == userNumber.Length - 1) && temp != string.Empty)
+    {
+        int numberEnter = Convert.ToInt32(temp);
+        if (numberEnter > 0)
+            countPositive++;
+        temp = string.Empty;
+    }
+}
+
+Console.WriteLine($"Количество чисел больше 0: {countPositive}");
---
+++ Task_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая найдет точку пересечения двух прямых, заданных уравнениями
+//y = k1*x + b1,   y = k2*x + b2;
+//значения b1,k1, b2 и k2 задаются пользователем.
+
+double b1 = inputValue("b1");
+double k1 = inputValue("k1");
+double b2 = inputValue("b2");
+double k2 = inputValue("k2");
+
+double inputValue(string value)
+{
+    Console.WriteLine($"Введение значение {value}: ");
+    return Convert.ToDouble(Console.ReadLine());
+}
+if(k1!=k2)
+{
+    double x = (b2-b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения двух прямых {x} {y}");
+}
+else
+{
+    if(b1 == b2)
+    {
+        Console.WriteLine("Прямые совпадают");
+    }
+    else
+    {
+       Console.WriteLine("Прямые параллельны"); 
+    }
+}
+
+
+
+
